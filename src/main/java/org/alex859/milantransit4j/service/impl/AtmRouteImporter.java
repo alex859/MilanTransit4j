5bb0cca00ef36549840b9ed55518706e82649143
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2015-06-09 00:42:49 +0100

    More work trying to deal with junctions

--- src/main/java/org/alex859/milantransit4j/service/impl/AtmRouteImporter.java
+++ src/main/java/org/alex859/milantransit4j/service/impl/AtmRouteImporter.java
@@ -17,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,7 +46,7 @@ public class AtmRouteImporter implements RouteImporter
 
       final AllRoutesResponse allRoutesResponse = getResponse(url, AllRoutesResponse.class);
 
-      allRoutesResponse.getJourneyPatterns().parallelStream()
+      allRoutesResponse.getJourneyPatterns().stream()
               .filter(jp -> jp != null)
               .forEach(this::process);
    }
@@ -66,17 +66,19 @@ public class AtmRouteImporter implements RouteImporter
    protected void process(final RouteDetails routeDetails)
    {
       LOGGER.info("Processing route {}", routeDetails.getLine().getLineDescription());
-      final Map<String, Map<Point, Stop>> segmentPointToStopMap = routeDetails.getGeometry().getSegments().stream()
-              .collect(toMap(this::getSegmentKey, s ->
-                              stopsToClosestPointMap(routeDetails.getStops(), s.getPoints()))
-              );
+      final List<Point> allPoints = routeDetails.getGeometry().getSegments().stream()
+              .flatMap(s -> s.getPoints().stream())
+              .collect(toList());
+
+      final Map<Point, Stop> pointToStopMap = getPointToClosestStopMap(routeDetails.getStops(), allPoints);
+      final Map<Segment, Map<Point, Stop>> additionalPointToStopBySegmentMap = getAdditionalPointToStopMap(routeDetails.getGeometry().getSegments(), pointToStopMap);
 
       for (final Segment segment : routeDetails.getGeometry().getSegments())
       {
+         final Map<Point, Stop> additionalPointToStopMap = additionalPointToStopBySegmentMap.get(segment);
          Stop lastStop = null;
          Point lastPoint = null;
          Double distance = 0.;
-         final Map<Point, Stop> pointToStopMap = segmentPointToStopMap.get(getSegmentKey(segment));
          for (final Point point : segment.getPoints())
          {
             if (lastPoint != null)
@@ -85,15 +87,20 @@ public class AtmRouteImporter implements RouteImporter
             }
             lastPoint = point;
 
-            final Stop currentStop = pointToStopMap.get(point);
+            Stop currentStop = pointToStopMap.get(point);
+            if (currentStop == null)
+            {
+               // check if we have it in the additional ones
+               currentStop = additionalPointToStopMap.get(point);
+            }
             if (currentStop != null)
             {
                // should not change that much, but we need it for junction, like to compute the correct distance
-               // between Pagano and Buonarroti
+               // between Pagano and Wagner
                distance += distance(currentStop, lastPoint);
                if (lastStop != null)
                {
-                  LOGGER.debug("{} -({})-> {}", lastStop.getDescription(), distance, currentStop.getDescription());
+                  LOGGER.info("{} -({})-> {}", lastStop.getDescription(), distance, currentStop.getDescription());
                   final StopNode lastStopNode = graphPersister.getOrCreateStopNode(lastStop, routeDetails);
                   final StopNode currentStopNode = graphPersister.getOrCreateStopNode(currentStop, routeDetails);
                   final RouteSegmentRelationship routeSegmentRelationship = graphPersister.createRouteSegmentRelationship(lastStopNode, currentStopNode, routeDetails, distance);
@@ -111,6 +118,46 @@ public class AtmRouteImporter implements RouteImporter
       routeDetails.getStops().stream()
               .filter(s -> s != null)
               .forEach(this::process);
+   }
+
+   /**
+    * We want to check that the first and last point of each segment map to a stop.
+    * If this is not true we make it map to the closest stop of the other segments.
+    * In this way we can deal with Pagano -> Buonarroti issue and probably more.
+    *
+    * @param segments The segments to be processed
+    * @param pointToStopMap The current point to stop map
+    * @return The point to stop entries to be added, grouped by segment
+    */
+   protected Map<Segment, Map<Point, Stop>> getAdditionalPointToStopMap(final List<Segment> segments, final Map<Point, Stop> pointToStopMap)
+   {
+      final Map<Segment, Map<Point, Stop>> map = new HashMap<>();
+
+      for (final Segment segment : segments)
+      {
+         final Map<Point, Stop> pointSegmentMap = new HashMap<>();
+         final Point firstPoint = segment.getPoints().get(0);
+         final Point lastPoint = segment.getPoints().get(segment.getPoints().size() - 1);
+         pointSegmentMap.put(firstPoint, findClosestStopNotInSegment(firstPoint, segment, pointToStopMap));
+         pointSegmentMap.put(lastPoint, findClosestStopNotInSegment(lastPoint, segment, pointToStopMap));
+         map.put(segment, pointSegmentMap);
+      }
+
+      return map;
+   }
+
+   protected Stop findClosestStopNotInSegment(final Point point, final Segment segment, final Map<Point, Stop> pointToStopMap)
+   {
+      final Stop currentClosestStop = pointToStopMap.get(point);
+      if (currentClosestStop == null)
+      {
+         return pointToStopMap.entrySet().stream()
+                 .filter(e -> !segment.getPoints().contains(e.getKey()))
+                 .map(Map.Entry::getValue)
+                 .min((s1, s2) -> distance(s1, point).compareTo(distance(s2, point))).get();
+      }
+
+      return currentClosestStop;
    }
 
    protected StopPointDistanceWrapper findClosestPointAndDistance(final Stop stop, final List<Point> points)
@@ -157,16 +204,12 @@ public class AtmRouteImporter implements RouteImporter
       LOGGER.debug("Processing TimeTables {}-{}-{}", t.getStopCode(), t.getLineCode(), t.getDirection());
    }
 
-   protected Map<Point, Stop> stopsToClosestPointMap(final List<Stop> stops, List<Point> points)
+   protected Map<Point, Stop> getPointToClosestStopMap(final List<Stop> stops, List<Point> points)
    {
-      return stops.parallelStream()
-              .map(s -> findClosestPointAndDistance(s, points))
-              .filter(w -> w.getDistance() < 250)
-              .collect(groupingBy(StopPointDistanceWrapper::getPoint))
-              .values().stream()
-              .map(l -> l.stream()
-                      .min((v1, v2) -> v1.getDistance().compareTo(v2.getDistance())).get())
-              .collect(toMap(StopPointDistanceWrapper::getPoint, StopPointDistanceWrapper::getStop));
+      return stops.stream()
+              .collect(toMap(Function.identity(), s -> points.stream().min((p1, p2) -> distance(s, p1).compareTo(distance(s, p2))).get()))
+              .entrySet().stream()
+              .collect(toMap(Map.Entry::getValue, Map.Entry::getKey));
    }
 
    protected Double distance(final Stop stop, final Point point)
@@ -255,6 +298,36 @@ public class AtmRouteImporter implements RouteImporter
    public void setGraphPersister(final GraphPersister graphPersister)
    {
       this.graphPersister = graphPersister;
+   }
+
+   protected class PointSegmentStopWrapper
+   {
+      private final Point point;
+      private final Segment segment;
+      private final Stop stop;
+
+      public PointSegmentStopWrapper(final Point point, final Segment segment, final Stop stop)
+      {
+         this.point = point;
+         this.segment = segment;
+         this.stop = stop;
+
+      }
+
+      public Point getPoint()
+      {
+         return point;
+      }
+
+      public Segment getSegment()
+      {
+         return segment;
+      }
+
+      public Stop getStop()
+      {
+         return stop;
+      }
    }
 
    protected static class StopPointDistanceWrapper
